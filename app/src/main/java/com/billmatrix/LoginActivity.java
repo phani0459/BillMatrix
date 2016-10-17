b358478d8b942bd670ba625164da81ac57996794
reventhk <revanthkandibanda@gmail.com>
2016-10-18 00:07:48 +0530

    test commit

--- app/src/main/java/com/billmatrix/LoginActivity.java
+++ app/src/main/java/com/billmatrix/LoginActivity.java
@@ -1,7 +1,8 @@
 package com.billmatrix;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
     }
 }

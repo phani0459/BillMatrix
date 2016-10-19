73e110ff4f9e5d3f820423201760522526c5f04a
Phani K <phani.0459@gmail.com>
2016-10-19 19:00:20 +0530

    Login UI Corrections

---
+++ app/src/main/java/com/billmatrix/BaseTabActivity.java
@@ -0,0 +1,106 @@
+package com.billmatrix;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.billmatrix.R;
+
+import java.io.File;
+
+public abstract class BaseTabActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    LinearLayout linearLayout;
+    Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_tab);
+        mContext = this;
+        textView = (TextView) findViewById(R.id.helloWorld);
+        linearLayout = (LinearLayout) findViewById(R.id.layout);
+
+        File file = new File(getExternalFilesDir(null), "pic");
+    }
+
+    public LinearLayout.LayoutParams getLayoutParams(int level) {
+        final LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        switch (level) {
+            case 0:
+                layoutParams.rightMargin = 12;
+                break;
+            case 1:
+                layoutParams.rightMargin = 12;
+                break;
+        }
+        return layoutParams;
+    }
+
+
+    public String getResString(int id) {
+        return getResources().getString(id);
+    }
+
+    public void addTabButtons(int n, String... names) {
+
+        if (n > 0) {
+            for (int i = 0; i < n; i++) {
+                LayoutInflater layoutInflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+                final Button button = (Button) layoutInflater.inflate(R.layout.tab_button, null);
+                button.setText(names[i]);
+
+                if (i != 0) {
+                    button.getBackground().setLevel(1);
+                    button.setTextColor(getResources().getColor(android.R.color.white));
+                    button.setLayoutParams(getLayoutParams(1));
+                } else {
+                    button.getBackground().setLevel(0);
+                    button.setTextColor(getResources().getColor(android.R.color.black));
+                    button.setLayoutParams(getLayoutParams(0));
+                    selectedTab = button.getText().toString();
+                    tabChanged(button.getText().toString(), true);
+                }
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (!selectedTab.equalsIgnoreCase(button.getText().toString())) {
+                            selectedTab = button.getText().toString();
+                            if (linearLayout.getChildCount() > 0) {
+                                for (int j = 0; j < linearLayout.getChildCount(); j++) {
+                                    View view = linearLayout.getChildAt(j);
+                                    view.getBackground().setLevel(1);
+                                    ((Button) view).setTextColor(getResources().getColor(android.R.color.white));
+                                    view.setLayoutParams(getLayoutParams(1));
+                                }
+                            }
+                            v.getBackground().setLevel(0);
+                            ((Button) v).setTextColor(getResources().getColor(android.R.color.black));
+                            v.setLayoutParams(getLayoutParams(0));
+
+                            tabChanged(((Button) v).getText().toString(), false);
+                        }
+                    }
+                });
+                linearLayout.addView(button);
+            }
+        }
+    }
+
+    String selectedTab;
+
+    public abstract void tabChanged(String selectedTab, boolean isInit);
+
+    public void setTextView(String msg) {
+        this.textView.setText(msg);
+    }
+
+}
--- app/src/main/java/com/billmatrix/LoginActivity.java
+++ app/src/main/java/com/billmatrix/LoginActivity.java
@@ -1,17 +1,33 @@
 package com.billmatrix;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = LoginActivity.class.getSimpleName();
+    private Context mContext;
+
+
+    @Bind(R.id.copyrightTextView)
+    public TextView copyrightTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
 
+        mContext = this;
+        ButterKnife.bind(this);
+
+        copyrightTextView.setText(getString(R.string.copyright, Calendar.getInstance().get(Calendar.YEAR)));
     }
 }
